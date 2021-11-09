9afcf1f5f06a453443bc83b68f6911ba7b50564b
MCCiupek <marieclemence.c@gmail.com>
2021-11-09 19:11:27 +0100

    day01/ex02

--- Module01/ex02/vector.py
+++ Module01/ex02/vector.py
@@ -17,13 +17,6 @@ def check_type(values, typ):
     return any(isinstance(val, typ) for val in values)
 
 
-def transpose(list_x):
-    return [[x] for x in list_x]
-
-def transpose_col(list_x):
-    return [x[0] for x in list_x]
-
-
 class Vector:
     def __init__(self, args=None):
         if args is None:
@@ -42,12 +35,19 @@ class Vector:
         self.shape = [0, 0]
 
     def __init_list__(self, values:list):
-        self.values = values
-        n_rows = len(values) if type(values[0]) == list else 1
-        n_cols = len(values[0]) if type(values[0]) == list else len(values)
-        self.shape = [n_rows, n_cols]
-        if not self.check_type(float):
-            raise ValueError("Values must be only floats")
+        try:
+            self.values = values
+            if type(values[0]) == list:
+                if len(values[0]) > 1:
+                    raise ValueError("Incorrect dimensions")
+                self.shape = [len(values), 1]
+            else:
+                self.shape = [1, len(values)]
+            if not self.check_type(float):
+                raise ValueError("Values must be only floats")
+        except ValueError as err:
+            sys.stderr.write("Error: {0}\n".format(err))
+            sys.exit()
 
     def __init_size__(self, size:int):
         self.values = [ [float(x)] for x in range(0, size) ]
@@ -68,18 +68,22 @@ class Vector:
             return any(check_type(self.values[i], float) for i in range(0, self.shape[0]))
 
     def __add__(self, vec):
-        if not isinstance(vec, Vector):
-            raise ValueError("Add/Sub with non vector object.")
+        try:
+            if not isinstance(vec, Vector):
+                raise ValueError("Add/Sub with non vector object.")
+                return None
+            if self.shape != vec.shape:
+                raise ValueError("Add/Sub with mismatching dimensions.")
+                return None
+            elif self.shape[0] == 0:
+                return Vector()
+            elif self.shape[0] == 1:
+                return Vector(add_lists(self.values, vec.values))
+            else:
+                return Vector([add_lists(self.values[i], vec.values[i]) for i in range(0, self.shape[0])])
+        except ValueError as err:
+            sys.stderr.write("Error: {0}\n".format(err))
             return None
-        if self.shape != vec.shape:
-            raise ValueError("Add/Sub with mismatching dimensions.")
-            return None
-        elif self.shape[0] == 0:
-            return Vector()
-        elif self.shape[0] == 1:
-            return Vector(add_lists(self.values, vec.values))
-        else:
-            return Vector([add_lists(self.values[i], vec.values[i]) for i in range(0, self.shape[0])])
 
     def __radd__(self, vec):
         return vec.__add__(self)
@@ -92,36 +96,42 @@ class Vector:
 
     def __mul__(self, scalar):
         try:
-            float(scalar)
-        except ValueError:
-            raise ValueError("Mult/Div by non scalar object.")
+            try:
+                float(scalar)
+            except ValueError:
+                raise ValueError("Mult/Div by non scalar object.")
+            if self.shape[0] == 0:
+                return Vector()
+            elif self.shape[0] == 1:
+                return Vector(mul_lists(self.values, float(scalar)))
+            else:
+                return Vector([mul_lists(self.values[i], float(scalar)) for i in range(0, self.shape[0])])
+        except ValueError as err:
+            sys.stderr.write("Error: {0}\n".format(err))
             return None
-        if self.shape[0] == 0:
-            return Vector()
-        elif self.shape[0] == 1:
-            return Vector(mul_lists(self.values, float(scalar)))
-        else:
-            return Vector([mul_lists(self.values[i], float(scalar)) for i in range(0, self.shape[0])])
-    
+
     def __rmul__(self, scalar):
         return self * scalar
 
     def __truediv__(self, scalar):
         try:
-            float(scalar)
-        except ValueError:
-            raise ValueError("__div__: Division by non scalar object.")
+            try:
+                float(scalar)
+            except ValueError:
+                raise ValueError("__div__: Division by non scalar object.")
+            if scalar == 0:
+                raise ValueError("__div__: Division by zero.")
+                return None
+            if self.shape[0] == 0:
+                return Vector()
+            elif self.shape[0] == 1:
+                return Vector(mul_lists(self.values, 1 / float(scalar)))
+            else:
+                return Vector([mul_lists(self.values[i], 1 / float(scalar)) for i in range(0, self.shape[0])])
+        except ValueError as err:
+            sys.stderr.write("Error: {0}\n".format(err))
             return None
-        if scalar == 0:
-            raise ValueError("__div__: Division by zero.")
-            return None
-        if self.shape[0] == 0:
-            return Vector()
-        elif self.shape[0] == 1:
-            return Vector(mul_lists(self.values, 1 / float(scalar)))
-        else:
-            return Vector([mul_lists(self.values[i], 1 / float(scalar)) for i in range(0, self.shape[0])])
-    
+
     def __rtruediv__(self, scalar):
         return self / scalar
 
@@ -132,26 +142,26 @@ class Vector:
         return "vector.Vector({0})".format(self.values)
 
     def dot(self, vec):
-        if not isinstance(vec, Vector):
-            raise ValueError("Dot product with non vector object.")
-        elif self.shape != vec.shape:
-            raise ValueError("Dot product with mismatching dimensions.")
-        elif self.shape[0] == 0:
+        try:
+            if not isinstance(vec, Vector):
+                raise ValueError("Dot product with non vector object.")
+            elif self.shape != vec.shape:
+                raise ValueError("Dot product with mismatching dimensions.")
+            elif self.shape[0] == 0:
+                return self
+            elif self.shape[0] == 1:
+                self.values = dot_prod(self.values, vec.values)
+            else:
+                self.values = [dot_prod(self.values[i], vec.values[i]) for i in range(0, self.shape[0])]
             return self
-        elif self.shape[0] == 1:
-            self.values = dot_prod(self.values, vec.values)
-        else:
-            self.values = [dot_prod(self.values[i], vec.values[i]) for i in range(0, self.shape[0])]
-        return self
+        except ValueError as err:
+            sys.stderr.write("Error: {0}\n".format(err))
+            return self
     
     def T(self):
         if self.shape[0] == 1:
-            self.values = transpose(self.values)
+            self.values = [[x] for x in self.values]
         if self.shape[1] == 1:
-            self.values = transpose_col(self.values)
-        else:
-            val = []
-            val = [val + transpose(self.values[i]) for i in range(0, self.shape[0])]
-            self.values = val
+            self.values = [x[0] for x in self.values]
         self.shape = self.shape[::-1]
         return self
