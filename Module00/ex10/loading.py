2aa5f0d4e45190ee05c2e76bf6c6cab3d92a7236
MCC <marieclemence.c@gmail.com>
2021-11-09 01:55:20 +0100

    day00/ex10

--- Module00/ex10/loading.py
+++ Module00/ex10/loading.py
@@ -2,39 +2,46 @@ import time
 import timeit
 import sys
 
-def ft_progress(listy):
-    start = timeit.default_timer()
-    elapsed_time = timeit.default_timer() - start
-    toolbar_width = 60
-    size = len(listy)
-    struct = "ETA: {0:.2f}s [{1: >2}%]".format(0, 0)
-    sys.stdout.write("ETA: {0:.2f}s [{1: >2}%][{2}] {3}/{4} | elapsed time {5:.2f}s".format(0, 0," " * toolbar_width, 0, size, elapsed_time))
+
+def ft_print_bar(j, i, width, size, elapsed_time):
+    percent = j / width
+    eta = elapsed_time / percent * (1 - percent)
+    s = "ETA: {0:.2f}s [{1: >2.0f}%][{2}] {3}/{4} | elapsed time {5:.2f}s"
+    bar = s.format(eta,
+                   percent * 100,
+                   "=" * (j - 1) + ">" * (i != size) + " " * (width - j),
+                   i,
+                   size,
+                   elapsed_time)
     sys.stdout.flush()
-    sys.stdout.write("\b" * (toolbar_width+1+16*2-4))
+    sys.stdout.write("\r" + bar)
+    sys.stdout.flush()
+
+
+def ft_progress(listy, width=60):
+    start = timeit.default_timer()
+    size = len(listy)
     j = 0
     for i in listy:
         yield i
-        elapsed_time = timeit.default_timer() - start
-        if (i / size * toolbar_width > j + 1):
+        if (i / size * width >= j + 1):
             j += 1
-            sys.stdout.write(">")
-            sys.stdout.write("\b")
-            sys.stdout.write("=")
-            sys.stdout.flush()
-    sys.stdout.write("=\n")
+        ft_print_bar(j + 1, i + 1, width, size,
+                     timeit.default_timer() - start)
 
-listy = range(1000)
-ret = 0
-for elem in ft_progress(listy):
-    ret += (elem + 3) % 5
-    time.sleep(0.01)
-print()
-print(ret)
 
-# listy = range(3333)
+# listy = range(1000)
 # ret = 0
 # for elem in ft_progress(listy):
-#     ret += elem
-#     time.sleep(0.005)
+#     ret += (elem + 3) % 5
+#     time.sleep(0.01)
 # print()
 # print(ret)
+
+listy = range(3333)
+ret = 0
+for elem in ft_progress(listy):
+    ret += elem
+    time.sleep(0.005)
+print()
+print(ret)
