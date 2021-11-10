6c47a7010822463db1dadc4159aa466fb96f0c1d
MCCiupek <marieclemence.c@gmail.com>
2021-11-10 13:48:54 +0100

    day02/ex01

---
+++ Module02/ex02/main.py
@@ -0,0 +1,47 @@
+def what_are_the_vars(*args, **kwargs):
+    """
+    ...
+    """
+    # print(args)
+    # print(kwargs)
+    obj = ObjectC()
+    if args:
+        keys = ["var_" + str(i) for i in range(0, len(args))]
+        for key, value in zip(keys, args):
+            setattr(obj, key, value)
+    if kwargs:
+        for key in kwargs.keys():
+            if key.startswith('var_'):
+                return None
+            setattr(obj, key, kwargs[key])
+    return obj
+
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
