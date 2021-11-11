43e0cf9c7d463418a6e03e4a184d89e9acca9917
MCCiupek <marieclemence.c@gmail.com>
2021-11-11 17:32:48 +0100

    day03/ex02

---
+++ Module03/ex01/ImageProcessor.py
@@ -0,0 +1,47 @@
+#import imageio
+import sys
+from matplotlib import image
+from matplotlib import pyplot as plt
+
+class ImageProcessor():
+    def load(self, path):
+        try:
+            array = image.imread(path)
+            print("Loading image of dimensions {0} x {1}".format(*array.shape))
+            return array
+        except FileNotFoundError as err:
+            print("Exception: FileNotFoundError -- strerror: {0}".format(err.strerror))
+            return None
+
+    def display(self, array):
+        plt.imshow(array)
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    from ImageProcessor import ImageProcessor
+
+    imp = ImageProcessor()
+    arr = imp.load("non_existing_file.png")
+    # Output
+    # Exception: FileNotFoundError -- strerror: No such file or directory
+
+    print(arr)
+    # Output
+    # None
+
+    arr = imp.load("empty_file.png")
+    # Output
+    # Exception: OSError -- strerror: None
+
+    print(arr)
+    # Output
+    # None
+
+    arr = imp.load("../resources/42AI.png")
+    # Output
+    # Loading image of dimensions 200 x 200
+    print(arr)
+    
+    imp.display(arr) 
