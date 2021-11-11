78315a423b955d9ebc2b470720269db06122de63
MCCiupek <marieclemence.c@gmail.com>
2021-11-11 10:54:09 +0100

    my_minipack

--- Module02/ex04/my_minipack_git/setup.py
+++ Module02/ex04/my_minipack_git/setup.py
@@ -10,11 +10,8 @@ setup(
   author_email = 'mciupek@student.42.fr',
   url = 'https://github.com/MCCiupek/my_minipack',
   download_url = 'https://github.com/MCCiupek/my_minipack/archive/pypi-0_1_3.tar.gz',
-  keywords = ['scraping', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
-  install_requires=[
-          'validators',
-          'beautifulsoup4',
-      ],
+  keywords = ['logging', 'log', 'bar', 'progressbar', 'progress'],
+  install_requires=[],
   classifiers=[  # Optional
     # How mature is this project? Common values are
     #   3 - Alpha
@@ -24,16 +21,17 @@ setup(
 
     # Indicate who your project is intended for
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
+    'Intended Audience :: Students',
+    'Topic :: Software Development :: Education',
+    'Topic :: Software Development :: HowTo',
+    'Topic :: Software Development :: Package',
 
     # Pick your license as you wish
-    'License :: OSI Approved :: MIT License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3 :: Only',
   ],
 )
