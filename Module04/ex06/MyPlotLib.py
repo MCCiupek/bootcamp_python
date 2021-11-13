0028eb50f854737cfc7bafc4536ebb0532d21d5c
MCCiupek <marieclemence.c@gmail.com>
2021-11-13 14:38:34 +0100

    day04/ex06

---
+++ Module04/ex06/MyPlotLib.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import scipy
+import seaborn
+import matplotlib.pyplot as plt
+
+
+class MyPlotLib():
+    """
+    This class implements different plotting methods
+    """
+
+    @staticmethod
+    def histogram(data, features):
+        """
+        plots one histogram for each numerical feature in
+        the list
+        """
+        df_filtered = data[features]
+        df_filtered.hist()
+
+    @staticmethod
+    def density(data, features):
+        """
+        plotss the density curve for each numerical feature in
+        the list
+        """
+        df_filtered = data[features]
+        df_filtered.plot.kde()
+
+    @staticmethod
+    def pair_plot(data, features):
+        """
+        plots a matrix of subplots (also called scatter plot
+        matrix). On each subplot shows a scatter plot of one numerical variable against
+        another one. The main diagonal of this matrix shows simple histograms.
+        """
+        df_filtered = data[features]
+        seaborn.pairplot(df_filtered)
+    
+    @staticmethod
+    def box_plot(data, features):
+        """
+        displays a box plot for each numerical variable in the
+        dataset.
+        """
+        df_filtered = data[features]
+        df_filtered.boxplot()
+
+
+if __name__=="__main__":
+
+    from FileLoader import FileLoader
+
+    loader = FileLoader()
+    data = loader.load('../data/athlete_events.csv')
+    loader.display(data, 12)
+    features = data.columns
+
+    mpl = MyPlotLib()
+
+    mpl.histogram(data, features)
